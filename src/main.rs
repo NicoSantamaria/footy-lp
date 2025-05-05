d3d57a09d46b34c44240a4a3bf6c4c9bfb2a179a
NicoSantamaria <nicoasantamaria@gmail.com>
2025-05-04 18:47:00 -0700

    build infinite capacity edges

--- src/main.rs
+++ src/main.rs
@@ -48,38 +48,66 @@ struct Node {
     capacity: u32
 }
 
-struct Edge {
-    source_team: Team,
-    target_team: Team,
-    capacity: u32
+struct Edge<'a> {
+    source_team: HashSet<&'a Team>,
+    target_team: HashSet<&'a Team>,
+    capacity: Option<u32>,
 }
 
-struct Graph {
-    nodes: Vec<Node>,
-    edges: Vec<Edge>,
-}
+// struct Graph {
+//     nodes: Vec<Node>,
+//     edges: Vec<Edge>,
+// }
 
+// TODO: add this function to its own file, split up into smaller
+// functions for easier testing
 fn build_constraints<'a>(
-    team: &'a Team,
-    opponents: Vec<&'a Team>,
+    source: &'a Team,
+    teams: Vec<&'a Team>,
     remaining_games: Vec<Game<'a>>,
 ) {
-    let possible_games: Vec<Game<'a>> = opponents
+    let possible_games_nodes: Vec<Game<'a>> = teams
         .iter()
         .combinations(2)
         .map(|combo| Game::new(combo[0], combo[1], 0))
         .collect();
 
-    let remaining_points_sources: Vec<RemainingPointsSource> = possible_games
+    let remaining_points_edges: Vec<RemainingPointsSource> = possible_games_nodes
         .iter()
         .map(|possible_game| {
             let remaining = remaining_games
                 .iter()
                 .filter(|game| game.teams == possible_game.teams)
                 .count() as u32 * 3;
-            RemainingPointsSource::new(team, possible_game, remaining)
+            RemainingPointsSource::new(source, possible_game, remaining)
         })
         .collect();
+
+    let mut infinite_capacity_edges: Vec<Edge> = Vec::new();
+    for source in &possible_games_nodes {
+        for &target in &teams {
+            if source.teams.contains(&target) {
+                let mut target_team = HashSet::new();
+                target_team.insert(target);
+
+                infinite_capacity_edges.push(Edge {
+                    source_team: source.teams.clone(), // Clone the reference set
+                    target_team,
+                    capacity: None,
+                });
+            }
+        }
+    }
+
+    for ice in infinite_capacity_edges {
+        println!("{:?}", ice.source_team);
+        println!("{:?}", ice.target_team);
+        println!("{:?}", ice.capacity);
+    }
+    // let remaining_distance_edges = vec![];
+
+
+    // Graph { nodes: vec![], edges: vec![] }
 }
 
 fn main() {
