ea452b29e1994d9329342e7bd9b5207871f12e96
NicoSantamaria <nicoasantamaria@gmail.com>
2025-05-14 21:01:41 -0700

    delete commented code

--- src/graph.rs
+++ src/graph.rs
@@ -51,46 +51,11 @@ impl Node {
             edge.target.borrow().traverse(f, seen);
         }
     }
-
-    // fn first(&self) -> Rc<RefCell<Node>> {
-    //     self.edges[0].clone()
-    // }
 }
+
 fn foo(node: &Node) {
     println!("foo: {:#?}", node.datum);
 }
-
-// fn init() -> Rc<RefCell<Node>> {
-//     let root = Node::new("A");
-//
-//     let b = Node::new("B");
-//     let c = Node::new("C");
-//     let d = Node::new("D");
-//     let e = Node::new("E");
-//     let f = Node::new("F");
-//
-//     {
-//         let mut mut_root = root.borrow_mut();
-//         mut_root.edges.push(b.clone());
-//         mut_root.edges.push(c.clone());
-//         mut_root.edges.push(d.clone());
-//
-//         let mut mut_c = c.borrow_mut();
-//         mut_c.edges.push(e.clone());
-//         mut_c.edges.push(f.clone());
-//         mut_c.edges.push(root.clone());
-//     }
-//
-//     root
-// }
-
-// pub fn main() {
-//     let g = init();
-//     let g = g.borrow();
-//     g.traverse(&|d| println!("{}", d), &mut HashSet::new());
-//     let f = g.first();
-//     foo(&*f.borrow());
-// }
 
 #[cfg(test)]
 mod tests {
